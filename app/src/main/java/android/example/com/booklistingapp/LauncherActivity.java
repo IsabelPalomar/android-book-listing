834075e4a594e1e51045194f230eaf6e0875c788
Isabel Palomar <isabel.palomarb@gmail.com>
2016-06-29 09:20:02 -0400

    starting point

---
+++ app/src/main/java/android/example/com/booklistingapp/LauncherActivity.java
@@ -0,0 +1,38 @@
+package android.example.com.booklistingapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class LauncherActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_launcher);
+
+        //Initiates the MainActivity after 1000L
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LauncherActivity.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+
+        }, 1000L);
+
+    }
+
+}
